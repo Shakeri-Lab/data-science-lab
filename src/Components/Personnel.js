dfddd52bbc2bf514fd4f3bb38c0fc9451677ad3e
Noah <noah.beamon648@gmail.com>
2021-01-19 18:01:55 -0500

    people mobile

--- src/Components/Personnel.js
+++ src/Components/Personnel.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import ReactMd from 'react-md-file';
 
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+  } from "react-device-detect";
+
 const Personnel = () => {
     return(
         <div className="App">
-            <p style={{margin: 20}}>Click to see a bio</p>
-            <div style={{display: "flex", flexDirection: "row"}}>
+            {!isMobile && <BrowserView>
+            <div style={{display: "flex", flexDirection: "row", overflow: "auto", margin: 20}}>
                 <div style={{display: "flex", flexDirection: "column", backgroundColor: "whitesmoke", borderRadius: 10, margin: 20, width: 250, cursor: "pointer"}}
                 onClick={() => {
                     window.location.href = "HemanShakeri"
@@ -29,6 +36,23 @@ const Personnel = () => {
                     </div>
                 </div>
             </div>
+            </BrowserView>}
+            {isMobile && <MobileView>
+                <div style={{marginBottom: 20, backgroundColor: "whitesmoke"}}>
+                    <img style={{heigth: window.innerHeight/2}} src="https://api.dsi.virginia.edu/sites/default/files/styles/square_sm/public/headshots/people/2020-01/Heman_001.png?w=1500" />
+                    <div style={{padding: 5}}>
+                        <strong>Heman Shakeri</strong>
+                        <p>Assistant Professor, School of Data Science | University of Virginia</p>
+                    </div>
+                </div>
+                <div style={{marginBottom: 20, backgroundColor: "whitesmoke"}}>
+                    <img style={{heigth: window.innerHeight/2}} src="https://virginiaequitycenter.org/sites/virginiaequitycenter.org/files/styles/people_crop__200x200_/public/Noah%20Beamon.png?itok=NoEZz7aZ" />
+                    <div style={{padding: 5}}>
+                        <strong>Noah Beamon</strong>
+                        <p>Undergraduate Research Assistant | University of Virginia</p>
+                    </div>
+                </div>
+            </MobileView>}
         </div>
     )
 }
