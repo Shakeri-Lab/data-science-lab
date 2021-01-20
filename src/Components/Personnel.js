ea176c3de4819676f1f6dbfb535d90bad722f4e5
Noah <noah.beamon648@gmail.com>
2021-01-19 19:26:48 -0500

    personnell updates

--- src/Components/Personnel.js
+++ src/Components/Personnel.js
@@ -11,7 +11,7 @@ import {
 const Personnel = () => {
     return(
         <div className="App">
-            {!isMobile && 
+            {!isMobile && <BrowserView>
             <div style={{display: "flex", flexDirection: "row", overflow: "auto", margin: 20}}>
                 <div style={{display: "flex", flexDirection: "column", backgroundColor: "whitesmoke", borderRadius: 10, margin: 20, width: 250, cursor: "pointer"}}
                 onClick={() => {
@@ -35,20 +35,35 @@ const Personnel = () => {
                         <p>Undergraduate Research Assistant | University of Virginia</p>
                     </div>
                 </div>
-            </div>}
-            {isMobile &&
-                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                    <div style={{margin: 30, backgroundColor: "whitesmoke", width: 250, borderRadius: 10}}>
-                        <img style={{heigth: 250, width: 20, borderRadius: 10}} src="https://api.dsi.virginia.edu/sites/default/files/styles/square_sm/public/headshots/people/2020-01/Heman_001.png?w=1500" />
+            </div>
+            </BrowserView>}
+            {isMobile && 
+            <MobileView>
+                <div style={{display: "flex", flexDirection: "column", overflow: "auto", margin: 20}}>
+                <div style={{display: "flex", flexDirection: "column", backgroundColor: "whitesmoke", borderRadius: 10, margin: 20, width: 250, cursor: "pointer"}}
+                onClick={() => {
+                    window.location.href = "HemanShakeri"
+                }}
+                >
+                    <img style={{borderRadius: 10, height: 250}} src="https://api.dsi.virginia.edu/sites/default/files/styles/square_sm/public/headshots/people/2020-01/Heman_001.png?w=1500" />
+                    <div style={{padding: 5}}>
                         <strong>Heman Shakeri</strong>
                         <p>Assistant Professor, School of Data Science | University of Virginia</p>
                     </div>
-                    <div style={{margin: 20, backgroundColor: "whitesmoke", width: 250, borderRadius: 10}}>
-                        <img style={{heigth: 250, width: 20, borderRadius: 10}} src="https://virginiaequitycenter.org/sites/virginiaequitycenter.org/files/styles/people_crop__200x200_/public/Noah%20Beamon.png?itok=NoEZz7aZ" />
+                </div>
+                <div style={{display: "flex", flexDirection: "column", backgroundColor: "whitesmoke", borderRadius: 10, margin: 20, width: 250, cursor: "pointer"}}
+                 onMouseClick={() => {
+                    
+                }}
+                >
+                    <img style={{borderRadius: 10, height: 250}} src="https://virginiaequitycenter.org/sites/virginiaequitycenter.org/files/styles/people_crop__200x200_/public/Noah%20Beamon.png?itok=NoEZz7aZ" />
+                    <div style={{padding: 5}}>
                         <strong>Noah Beamon</strong>
                         <p>Undergraduate Research Assistant | University of Virginia</p>
                     </div>
-                </div>}
+                </div>
+            </div>
+            </MobileView>}
         </div>
     )
 }
