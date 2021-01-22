5933203dd7c790078358c1de57d1049486ab2001
Noah <noah.beamon648@gmail.com>
2021-01-21 23:12:07 -0500

    personnell mobile support

--- src/Components/Personnel.js
+++ src/Components/Personnel.js
@@ -64,14 +64,22 @@ const Personnel = () => {
                 </div>
             </div> */}
             <div style={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                <div style={{borderRadius: 10, margin: 20}}>
+                <div style={{borderRadius: 10, margin: 20}}
+                onClick={() => {
+                    window.location.href = "HemanShakeri"
+                }}
+                >
                     <img style={{borderRadius: 10, height: 250}} src="https://api.dsi.virginia.edu/sites/default/files/styles/square_sm/public/headshots/people/2020-01/Heman_001.png?w=1500"/>
                     <div style={{padding: 5}}>
                         <strong>Heman Shakeri</strong>
                         <p>Assistant Professor, School of Data Science | University of Virginia</p>
                     </div>
                 </div>
-                <div style={{borderRadius: 10, margin: 20}}>
+                <div style={{borderRadius: 10, margin: 20}}
+                onClick={() => {
+                    
+                }}
+                >
                     <img style={{borderRadius: 10, height: 250}} src="https://virginiaequitycenter.org/sites/virginiaequitycenter.org/files/styles/people_crop__200x200_/public/Noah%20Beamon.png?itok=NoEZz7aZ"/>
                     <div style={{padding: 5}}>
                         <strong>Noah Beamon</strong>
